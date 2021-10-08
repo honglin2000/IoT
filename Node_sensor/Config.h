a3d495a4635ff7307ee2db435a06c123c5e21280
gerald-t <82646945+gerald-t@users.noreply.github.com>
2021-10-08 22:09:13 +0800

    Update Config.h

--- Node_sensor/Config.h
+++ Node_sensor/Config.h
@@ -2,6 +2,4 @@
 #define WIFI_SSID "SSID" // SSID of your WiFi network
 #define WIFI_PASSWORD "PASSWORD" // Password of your WiFi network
 
-#define NUM_PIXELS 140 // Number of pixels to display
-
 const int address[4] = { 0,0,0,0 }; // Address of your RPi, split into 4 parts
