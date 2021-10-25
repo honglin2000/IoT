3e26dbc1c822fea3c5c360a127a4a9ccaeb76ac4
gerald-t <82646945+gerald-t@users.noreply.github.com>
2021-10-25 22:56:19 +0800

    Update Node_sensor.ino

--- Node_sensor/Node_sensor.ino
+++ Node_sensor/Node_sensor.ino
@@ -1,48 +1,79 @@
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
+//#include <Adafruit_MPU6050.h>
+//#include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <WiFi.h>
 #include "Config.h"
+#define BUZZER_PIN 5 // Buzzer's pin 5
 #define WIFI_TIMEOUT_MS 20000
 
 IPAddress server(address[0], address[1], address[2], address[3]);
 WiFiClient client;
 
-Adafruit_MPU6050 mpu;
-sensors_event_t a, g, temp;
+//Adafruit_MPU6050 mpu;
+//sensors_event_t a, g, temp;
 
-//int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-//float ax=0, ay=0,az=0, gx=0,gy=0,gz=0;
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+static int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+float ax = 0, ay = 0, az = 0, gx = 0, gy = 0, gz = 0;
 boolean fall = false;
-boolean trigger1 = false, trigger2 = false, trigger3 = false;
-boolean trigCount1 = 0, trigCount2 = 0, trigCount3 = 0; //trigger Count
+boolean trigger1 = false, trigger2 = false, trigger3 = false;  //trigger
+byte trigCount1 = 0, trigCount2 = 0, trigCount3 = 0; //trigger Count
 int angleChange = 0;
-//void send_event(const char*event);
 
 void setup() {
   Serial.begin(9600);
-  //  pinMode(buzzer, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  Wire.begin(21, 22, 100000);
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip!");
-    while (1) {
-      delay(10);
+  /*
+    if (!mpu.begin()) {
+      Serial.println("Failed to find MPU6050 chip!");
+      while (1) {
+        delay(10);
+      }
     }
-  }
   Serial.println("MPU6050 Found!");
-  //mpu.getAccelerometerSensor();
+  */
 }
 
+/*
+ This program first checks if the accelerometer value is higher than the lower threshold, 
+ if so, then it waits for half a second and checks for higher thresholds. 
+ If the accelerometer value exceeds the upper threshold, 
+ then it calculates the change in orientation for the gyroscope values. 
+ When there is a sudden change in orientation, 
+ it waits for 10 seconds and checks if the orientation remains the same.
+ */
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     mpu_read();
-    float Amp = pow(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2), 0.5);
-    //int Amp = Raw_Amp * 10; // Mulitiplied by 10 bcz values are between 0 to 1
-    Serial.println(Amp);
-    //double mag = sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2));
-    //client.println(mag);
+    Serial.print(AcX); Serial.print(" ");
+    Serial.print(AcY); Serial.print(" "); Serial.println(AcZ);
+    ax = (AcX - 2050) / 16384.00;
+    ay = (AcY - 77) / 16384.00;
+    az = (AcZ - 1947) / 16384.00;
+    gx = (GyX + 270) / 131.07;
+    gy = (GyY - 351) / 131.07;
+    gz = (GyZ + 136) / 131.07;
 
-    if (Amp <= 2 && trigger2 == false) { 
+    //  ax = AcX / 16384.00;
+    //  ay = AcY / 16384.00;
+    //  az = AcZ / 16384.00;
+    //  gx = GyX / 131.07;
+    //  gy = GyY / 131.07;
+    //  gz = GyZ / 131.07;
+    Serial.print(ax); Serial.print(" ");
+    Serial.print(ay); Serial.print(" "); Serial.println(az);
+
+    float Raw_Amp = pow(pow(ax, 2) + pow(ay, 2) + pow(az, 2), 0.5);
+    float Amp = Raw_Amp * 10;  // Mulitiplied by 10 bcz values are between 0 to 1
+    Serial.print("Amp: "); Serial.println(Amp);
+
+    if (Amp <= 2 && trigger2 == false) {
       //if amplitude breaks lower threshold (0.4g)
       trigger1 = true;
       Serial.println("TRIGGER 1 ACTIVATED");
@@ -58,8 +89,10 @@ void loop() {
     }
     if (trigger2 == true) {
       trigCount2++;
-      angleChange = pow(pow(g.gyro.x, 2) + pow(g.gyro.y, 2) + pow(g.gyro.z, 2), 0.5); Serial.println(angleChange);
-      if (angleChange >= 30 && angleChange <= 400) {
+      angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
+      Serial.print("Angle-2: ");
+      Serial.println(angleChange);
+      if (angleChange >= 0.50 && angleChange <= 4.00) {
         //if orientation changes by between 80-100 degrees
         trigger3 = true; trigger2 = false; trigCount2 = 0;
         Serial.println(angleChange);
@@ -68,14 +101,14 @@ void loop() {
     }
     if (trigger3 == true) {
       trigCount3++;
-      if (trigCount3 >= 10) {
-        angleChange = pow(pow(g.gyro.x, 2) + pow(g.gyro.y, 2) + pow(g.gyro.z, 2), 0.5);
+      if (trigCount3 >= 20) {
+        angleChange = pow(pow(gx, 2) + pow(gy, 2) + pow(gz, 2), 0.5);
         //delay(10);
+        Serial.print("Angle-3: ");
         Serial.println(angleChange);
-        if ((angleChange >= 0) && (angleChange <= 10)) {
-          //if orientation changes remains between 0-10 degrees
+        if ((angleChange >= 0) && (angleChange <= 30)) {
+          //if orientation changes remains between 0-30 degrees
           fall = true; trigger3 = false; trigCount3 = 0;
-          Serial.println(angleChange);
         }
         else {
           //user regained normal orientation
@@ -87,9 +120,22 @@ void loop() {
     if (fall == true) {
       //in event of a fall detection
       Serial.println("FALL DETECTED");
-      //send_event("fall_detect");
+      digitalWrite(BUZZER_PIN, HIGH);
+      delay(3000);
+      digitalWrite(BUZZER_PIN, LOW);
       fall = false;
     }
+    if (trigCount1 >= 6) {
+      //allow 0.5s for AM to break upper threshold
+      trigger1 = false; trigCount1 = 0;
+      Serial.println("TRIGGER 1 DECACTIVATED");
+    }
+    if (trigCount2 >= 6) {
+      //allow 0.5s for orientation change
+      trigger2 = false; trigCount2 = 0;
+      Serial.println("TRIGGER 2 DECACTIVATED");
+    }
+    delay(100);
 
   } else if (WiFi.status() != WL_CONNECTED) {
     // (Re)Connect to WiFi network
@@ -105,23 +151,17 @@ void loop() {
 /////////////////////////////////////////////////////////////////////////////
 
 void mpu_read() {
-  mpu.getEvent(&a, &g, &temp);
-  //  Serial.print("Acceleration X: ");
-  //  Serial.print(a.acceleration.x);
-  //  Serial.print(", Y: ");
-  //  Serial.print(a.acceleration.y);
-  //  Serial.print(", Z: ");
-  //  Serial.print(a.acceleration.z);
-  //  Serial.println(" m/s^2");
-  //
-  //  Serial.print("Rotation X: ");
-  //  Serial.print(g.gyro.x);
-  //  Serial.print(", Y: ");
-  //  Serial.print(g.gyro.y);
-  //  Serial.print(", Z: ");
-  //  Serial.print(g.gyro.z);
-  //  Serial.println(" rad/s");
-  delay(1000);
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
 
 void connectToWiFi() {
