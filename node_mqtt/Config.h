19f09b7c288f0a4d86616e6f71154547c5b70f89
gerald-t <82646945+gerald-t@users.noreply.github.com>
2021-11-04 23:12:10 +0800

    Create Config.h

---
+++ node_mqtt/Config.h
@@ -0,0 +1,7 @@
+// Rename this file to "Config.h"
+#define WIFI_SSID "xxxxx" // SSID of your WiFi network
+#define WIFI_PASSWORD "xxxxx" // Password of your WiFi network
+#define MQTT_USER "username"
+#define MQTT_PASSWORD "12345678"
+
+const int address[4] = { 192, 168, 68, 113 }; // Address of your RPi, split into 4 parts
