3ef50db933b6cb517959fb86868b2496573b24d3
gerald-t <82646945+gerald-t@users.noreply.github.com>
2021-11-14 16:42:35 +0800

    Delete Config.h

--- node_mqtt/Config.h
+++
@@ -1,7 +0,0 @@
-// Rename this file to "Config.h"
-#define WIFI_SSID "xxxxx" // SSID of your WiFi network
-#define WIFI_PASSWORD "xxxxx" // Password of your WiFi network
-#define MQTT_USER "username"
-#define MQTT_PASSWORD "12345678"
-
-const int address[4] = { 0, 0, 0, 0 }; // Address of your RPi, split into 4 parts
