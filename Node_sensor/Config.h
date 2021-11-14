aa2055e4ce4cdf501e0bf12baf16629bf2c5d4b9
gerald-t <82646945+gerald-t@users.noreply.github.com>
2021-11-14 16:41:47 +0800

    Update Config.h

--- Node_sensor/Config.h
+++ Node_sensor/Config.h
@@ -1,5 +1,14 @@
 // Rename this file to "Config.h"
-#define WIFI_SSID "SSID" // SSID of your WiFi network
-#define WIFI_PASSWORD "PASSWORD" // Password of your WiFi network
+#define WIFI_SSID "change this" // SSID of your WiFi network
+#define WIFI_PASSWORD "change this" // Password of your WiFi network
+#define mqtt_server "192.168.68.116"  // IP of the MQTT broker
+#define status_topic "home/fall/status"
+#define fall_topic "home/fall/data"
+#define mqtt_username "iot" // MQTT username
+#define mqtt_password "iot" // MQTT password
+#define clientID "client_fall" // MQTT client ID
+#define host "maker.ifttt.com"
+#define privateKey "change this"
 
-const int address[4] = { 0,0,0,0 }; // Address of your RPi, split into 4 parts
+
+const int address[4] = { 192, 168, 68, 113 }; // Address of your RPi, split into 4 parts
